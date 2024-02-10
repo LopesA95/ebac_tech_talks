81805c8d25f4bef89752a81c458b8cdc2014d185
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-10 11:56:25 -0400

    feat: implementando o contador regressivo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 AOS.init();
+
+const dataDoEvento = new Date("feb 15, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+  const segundoEmMs = 1000;
+
+  const diasAteOEvento = Math.floor(distanciaDoEvento / diaEmMs);
+  const horasAteOEvento = Math.floor((distanciaDoEvento % diaEmMs) / horaEmMs);
+  const minutosAteOEvento = Math.floor(
+    (distanciaDoEvento % horaEmMs) / minutoEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaDoEvento % minutoEmMs) / segundoEmMs
+  );
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaDoEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("evento-iniciado").innerHTML =
+      "Desculpe, o evento ja iniciou.";
+    document.getElementById("modify").innerHTML = "Ingressos expirados";
+  }
+}, 1000);
